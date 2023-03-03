8df6c656b2fa6791fd8b313712eff3dd33d09ed2
Samuel Ume <cume357@gmail.com>
2023-03-03 11:54:23 +0100

    Added new feature: improved the navvbar design

--- src/components/molecule/Footer.js
+++ src/components/molecule/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <h1 >Footer</h1>
+    
     </div>
   )
 }
--- src/layout/main.js
+++ src/layout/main.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../components/molecule/Footer";
 import Navbar from "../components/molecule/Navbar";
 
 const MainLayout = ({ children }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (typeof window !== "undefined") {
+      // You now have access to `window`
+      const isTop = window.scrollY > 0;
+      setIsScrolled(isTop);
+    }
+  };
+  useEffect(() => {
+    console.log(window.scrollY);
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="container-class">
-      <Navbar />
+      <Navbar isScrolled={isScrolled} />
       <main>{children}</main>
       <Footer />
     </div>
