d1c4610ef27aacdaf1e4ef0ce396bae59da7000f
kaythatcodes <kayceepeters2nd@gmail.com>
2023-03-03 11:31:16 +0100

    Added new feature: "footer"

--- src/components/molecule/Footer.js
+++ src/components/molecule/Footer.js
@@ -3,8 +3,12 @@ import styles from "@/styles/Footer.module.css"
 
 const Footer = () => {
   return (
+    <div>
     <div className={styles.container}>
-      <h1 >Footer</h1>
+      <div className={styles.textarea}> 
+      <h3>fuck you 😂</h3>
+      </div>
+    </div>
     </div>
   )
 }
