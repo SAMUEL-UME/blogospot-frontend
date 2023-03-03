5a74018c88a52504e61efc310373f4c69c552adf
Samuel Ume <cume357@gmail.com>
2023-03-03 04:39:23 +0100

    Added new feature: increased length of get blogs body on "BlogBlock.js"

--- src/components/template/BlogBlock.js
+++ src/components/template/BlogBlock.js
@@ -10,7 +10,7 @@ const BlogBlock = ({ data }) => {
             <span key={tag}>#{tag}</span>
           ))}
           <h1 className={styles.blog_head}>{data.title}</h1>
-          <p>{data.body.slice(0, 15)}...</p>
+          <p>{data.body.slice(0, 100)}...</p>
           <div className={styles.meta_info}>
             <div>published by: {data.author.username}</div>
             <div>1min read</div>
