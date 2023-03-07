d314067accd8b0a93688c0455a88cef2e9bd56d3
Aro Fawaz <arofawaz123@gmail.com>
2023-03-07 13:38:41 +0100

    merge with the main branch

--- src/components/template/Login.js
+++ src/components/template/Login.js
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-
-export default function Login() {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
-    // Add logic to submit form data to backend API or database
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4 text-gray-700">Log In</h1>
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder= "Password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex items-center justify-between space-x-2">
-          <button
-            className="bg-orange-500 hover:bg-orange-700 text-gray-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Log In
-          </button>
-          <p>Don't have an account?</p>
-          <a href="" className="text-orange-800">
-            SignUp!
-          </a>
-        </div>
-      </form>
-=======
 import React from "react";
 import styles from "@/styles/auth/Login.module.css";
 import Link from "next/link";
@@ -103,7 +43,6 @@ export default function LoginPage({ handleInputChange, state }) {
           </Link>
         </p>
       </div>
->>>>>>> main
     </div>
   );
 }
