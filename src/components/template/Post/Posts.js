7c9ce0a20f8efab2e4e67ca6f2c4f769b3dcf8b9
Aro Fawaz <arofawaz123@gmail.com>
2023-03-13 13:48:04 +0100

    added sklenton loading

--- src/components/template/Post/Posts.js
+++ src/components/template/Post/Posts.js
@@ -8,8 +8,17 @@ import { RiHeart2Line } from "react-icons/ri";
 import { TbMessageCircle2 } from "react-icons/tb";
 
 const Posts = ({ data }) => {
+  const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 5000);
+    }, []);
   return (
     <div>
+    {!loading ? (
+      <React.Fragment>
       {data?.map((data) => (
         <div className={styles.blog} key={data._id}>
           <div className={styles.user}>
@@ -58,6 +67,32 @@ const Posts = ({ data }) => {
           </div>
         </div>
       ))}
+      </React.Fragment>
+  ) : (
+      <React.Fragment>
+      {Array(12)
+        .fill(0)
+        .map((_, i) => (
+            <div key={i} className="flex flex-col border border-gray-300 mb-4 rounded-lg shadow-sm animate-pulse">
+                <div className="bg-gray-200 flex flex-row p-4 pb-1">
+                   <div className="bg-gray-300 w-20 h-20 rounded-full justify-center items-center">                   
+                   </div>
+                   <div className="flex flex-col space-x-2 space-y-2">
+                   <div className="bg-gray-300 w-32 h-4 mt-6 ml-2 rounded-lg"></div>
+                   <div className="bg-gray-300 w-64 h-2 rounded-lg"></div>
+                   </div>
+                </div>
+                <div className="bg-gray-200 p-6 pt-0 space-y-4 justify-center items-center m-0">
+                  <div className="bg-gray-300 w-4/5 h-4 ml-20 pt-0 rounded-lg"></div>
+                  <div className="bg-gray-300 w-4/5 h-4 ml-20 pt-0 rounded-lg"></div>
+                  <div className="bg-gray-300 w-4/5 h-4 ml-20 pt-0  rounded-lg"></div>
+                  <div className="bg-gray-300 w-32 h-4 ml-20 pt-0  rounded-lg"></div>
+                  <div className="bg-gray-300 w-24 h-2 ml-20 rounded-lg"></div>
+                </div>
+              </div>
+            ))}
+      </React.Fragment>
+      )}
     </div>
   );
 };
