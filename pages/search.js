9dab01b71648625b16a963f9474430e24aa933df
Samuel Ume <cume357@gmail.com>
2023-04-19 17:07:55 +0100

    Added feature: New update tp search page

--- pages/search.js
+++ pages/search.js
@@ -5,10 +5,9 @@ import SearchPage from "@/src/components/SearchPage";
 import { useSelector, useDispatch } from "react-redux";
 import { sideBar } from "@/src/Redux/themeSlice.js";
 
-
-
 const { dark, search_container, light } = styles;
-export default function Search() {
+
+export default function Search({ data }) {
   const { search } = useSelector(({ search }) => search);
   const { theme } = useSelector(({ theme }) => theme);
   const dispatch = useDispatch();
@@ -16,89 +15,23 @@ export default function Search() {
     dispatch(sideBar());
   }, [dispatch]);
 
-
   return (
     <>
       <Head>
         <title>search</title>
       </Head>
       <div className={`${search_container} ${theme === "true" ? dark : light}`}>
-        <SearchPage search={search} sideMenu={sideMenu} />
+        <SearchPage search={search} sideMenu={sideMenu} data={data} theme={theme} />
       </div>
     </>
   );
 }
-/***import React, { useState } from "react";
 
-function App() {
-  const [selected, setSelected] = useState("Posts");
-
-  const handleSelection = (event) => {
-    setSelected(event.target.innerText);
+export async function getServerSideProps() {
+  const { data } = await import("/data/data.json");
+  return {
+    props: {
+      data,
+    },
   };
-
-  const posts = [
-    { id: 1, title: "First Post", content: "This is the first post." },
-    { id: 2, title: "Second Post", content: "This is the second post." },
-    { id: 3, title: "Third Post", content: "This is the third post." },
-  ];
-
-  const tags = [
-    { id: 1, name: "React" },
-    { id: 2, name: "JavaScript" },
-    { id: 3, name: "CSS" },
-  ];
-
-  const people = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Jane Doe" },
-    { id: 3, name: "Bob Smith" },
-  ];
-
-  const myPosts = [
-    { id: 1, title: "First Post", content: "This is the first post." },
-    { id: 3, title: "Third Post", content: "This is the third post." },
-  ];
-
-  let dataToRender;
-
-  switch (selected) {
-    case "Posts":
-      dataToRender = posts;
-      break;
-    case "Tags":
-      dataToRender = tags;
-      break;
-    case "People":
-      dataToRender = people;
-      break;
-    case "My post only":
-      dataToRender = myPosts;
-      break;
-    default:
-      dataToRender = posts;
-      break;
-  }
-
-  return (
-    <div>
-      <ul>
-        <li onClick={handleSelection}>Posts</li>
-        <li onClick={handleSelection}>Tags</li>
-        <li onClick={handleSelection}>People</li>
-        <li onClick={handleSelection}>My post only</li>
-      </ul>
-      <div>
-        {dataToRender.map((item) => (
-          <div key={item.id}>
-            <h2>{item.title || item.name}</h2>
-            <p>{item.content || item.name}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 }
-
-export default App;
- */
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,6 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useState } from "react";
 import styles from "@/styles/Search.module.css";
 import { useSelector } from "react-redux";
+import Posts from "../components/Post/Posts";
 
 const {
   container,
@@ -11,18 +12,45 @@ const {
   display_cat,
 } = styles;
 
-const SearchPage = ({ search, sideMenu }) => {
+const SearchPage = ({ search, sideMenu, data, theme }) => {
   const { menu } = useSelector((state) => state.theme);
+  const [selected, setSelected] = useState("Posts");
+  const handleSelection = (event) => {
+    setSelected(event.target.innerText);
+  };
+
+  let dataToRender;
+
+  switch (selected) {
+    case "Posts":
+      dataToRender = "posts";
+      break;
+    case "Tags":
+      dataToRender = "tags";
+      break;
+    case "People":
+      dataToRender = "people";
+      break;
+    case "My post only":
+      dataToRender = "myPosts";
+      break;
+    default:
+      dataToRender = "posts";
+      break;
+  }
+
   return (
     <div className={container}>
       <div className={containerHeaidng}>
-        <p>Search results for : <span> {search}</span></p>
+        <p>
+          Search results for : <span> {search}</span>
+        </p>
         <div>
           <ul>
             <li>Recent</li>
             <li>Oldest</li>
           </ul>
-        </div>
+        </div> 
       </div>
       <div className={containerContent}>
         <div
@@ -31,13 +59,18 @@ const SearchPage = ({ search, sideMenu }) => {
           } animate__animated animate__backInLeft`}
         >
           <ul>
-            <li>Posts</li>
-            <li>Tags</li>
-            <li>People</li>
-            <li>My post only</li>
+            <li onClick={handleSelection}>Posts</li>
+            <li onClick={handleSelection}>Tags</li>
+            <li onClick={handleSelection}>People</li>
+            <li onClick={handleSelection}>My post only</li>
           </ul>
         </div>
-        <div className={content}></div>
+        <div className={content}>
+          {dataToRender === "posts" && <Posts data={data} theme={theme} />}
+          {dataToRender === "people" && <h1>People</h1>}
+          {dataToRender === "tags" && <h1>Tags</h1>}
+          {dataToRender === "myPosts" && <h1>My post</h1>}
+        </div>
       </div>
     </div>
   );
